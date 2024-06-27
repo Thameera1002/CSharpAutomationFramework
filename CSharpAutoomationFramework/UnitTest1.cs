c29e02c311340f02c574c87db874bf0410291c11
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2024-06-27 19:30:43 +0530

    Add project files.

---
+++ CSharpAutoomationFramework/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CSharpAutoomationFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
